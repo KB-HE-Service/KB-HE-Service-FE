1bfc0cc45ec4c37c50ccb54a98ea226c99e0677e
selfishAltruism <officialkyus@gmail.com>
2024-07-27 18:18:45 +0900

    fix: apply git ignore

--- src/shared/hooks/services/AuthService.tsx
+++ src/shared/hooks/services/AuthService.tsx
@@ -1,28 +1,7 @@
 import { AxiosResponse } from "axios";
 import { useNavigate } from "react-router-dom";
 
-import { API, setAccess } from "@/shared/configs/axios";
+import { API, storeUserInfo, removeUserInfo, getUserInfo } from "@/shared";
 import { PAGE_URL } from "@/shared/configs/path";
 
-import { useUserStore } from "@/shared";
-
-export const AuthService = () => {
-  const signIn = useUserStore((state) => state.signIn);
-  const navigate = useNavigate();
-
-  const URL = "/api/v1/member";
-
-  const signin = async (body: User.SignInReqDto) => {
-    const { data } = (await API.post(
-      `${URL}/signIn`,
-      body
-    )) as AxiosResponse<User.SignInResDto>;
-
-    setAccess(data.id);
-    signIn({ ...data });
-
-    navigate(PAGE_URL.Setting);
-  };
-
-  return { signin };
-};
+export const AuthService = () => {};
