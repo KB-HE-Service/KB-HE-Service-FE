abe095c50e2c5b6d14e4ebd7e43174c567c83ccb
selfishAltruism <officialkyus@gmail.com>
2024-08-04 20:41:57 +0900

    design: trainning  page

--- src/entities/Line.tsx
+++ src/entities/Line.tsx
@@ -2,8 +2,8 @@ import styled from "@emotion/styled";
 
 export const MidPointLine = styled.div`
   border-top: 1px solid #776c61;
-  margin-top: 20px;
-  margin-bottom: 30px;
+  margin-top: 17px;
+  margin-bottom: 20px;
   width: 90%;
   height: 0px;
   color: #776c61;
--- src/pages/training/TrainingPage.tsx
+++ src/pages/training/TrainingPage.tsx
@@ -22,7 +22,11 @@ const TrainingPage = () => {
           <MidPointLine />
           <SubTitle>사용자에 가장 적합한 보험을 추천하는 AI입니다.</SubTitle>
           <SubTitle>하단 버튼을 누르면 AI 개발에 기여할 수 있습니다.</SubTitle>
-          <div style={{ height: "100px" }}></div>
+          <div style={{ height: "60px" }}></div>
+          <img
+            src="/img/character.png"
+            style={{ width: "120px", marginBottom: "10px" }}
+          />
           <Explanation>모든 학습 과정은 암호화를 통해서 이뤄지며,</Explanation>
           <Explanation>본 기기를 제외한 어떤 기기와 서버에서도</Explanation>
           <Explanation>
